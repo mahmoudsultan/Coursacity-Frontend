5c98cc16d9328154739b565d2770f7ccbcb09ea4
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2020-09-02 21:55:18 +0200

    New Course Form.

---
+++ src/components/courses/CourseForm.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-form v-model="isDataValid">
+    <v-container fluid>
+      <v-row>
+        <v-col cols="4">
+          <v-text-field
+            solo
+            :rules="titleRules"
+            name="title"
+            label="Title"
+            v-model="newCourse.name"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="8">
+          <v-textarea
+            solo
+            :rules="descriptionRules"
+            name="description"
+            label="Description"
+            v-model="newCourse.description"
+          ></v-textarea>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="4">
+          <v-text-field
+            solo
+            :rules="slugRules"
+            name="slug"
+            label="Slug"
+            v-model="newCourse.slug"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="4">
+          <v-file-input
+            solo
+            :rules="photoRules"
+            prepend-icon="fa-camera-retro"
+            accept="image/*"
+            label="Photo"
+            v-model="newCourse.photo"
+          ></v-file-input>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn color="success" x-large :disabled="!isDataValid" @click="submit">Create</v-btn>
+          <span class="ml-5" v-if="loading.isLoading">
+            <v-progress-circular :value="10" color="success" indeterminate></v-progress-circular>
+            <span class="text-body1 ml-2">{{ loading.message }}... </span>
+          </span>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Object,
+      default: () => ({
+        isLoading: false,
+      })
+    }
+  },
+  data () {
+    return {
+      isDataValid: false,
+      newCourse: {
+        name: '',
+        description: '',
+        slug: '',
+        photo: null
+      },
+      titleRules: [
+        v => !!v || 'Title is required',
+        v => v.length <= 100 || 'Name must be less than 100 characters',
+      ],
+      descriptionRules: [
+        v => !!v || 'Description is required',
+      ],
+      slugRules: [
+        v => !!v || 'Slug is required',
+      ],
+      photoRules: [
+        v => !!v || 'Photo is required',
+      ]
+    }
+  },
+  methods: {
+    submit() {
+      if (!this.isDataValid) return;
+      this.$emit('submit', this.newCourse);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/NewCourse.vue
+++ src/views/NewCourse.vue
@@ -1,10 +1,49 @@
 <template>
-  <h1>New Course</h1>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1 class="text-h1">New Course</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <CourseForm :loading="loading" @submit="submit" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
+import CourseForm from '@/components/courses/CourseForm.vue';
 
+export default {
+  components: {
+    CourseForm,
+  },
+  data() {
+    return {
+      loading: {
+        isLoading: false,
+        message: '',
+      },
+    };
+  },
+  methods: {
+    submit(newCourse) { // eslint-disable-line
+      // TODO
+      this.loading = {
+        isLoading: true,
+        message: 'Creating New Course',
+      };
+
+      const self = this;
+      setTimeout(() => {
+        self.loading = {
+          isLoading: false,
+        }
+      }, 1000);
+    }
+  }
 }
 </script>
 
